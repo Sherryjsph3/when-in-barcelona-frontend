2dbae90af96b0b3a9a6b90ac1d6abb9ade406db5
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2023-02-27 17:29:19 -0500

    updated responsive for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,17 +13,18 @@ function Home(props) {
             <h2 className='logo'>When in</h2>
             <h3 className='sub-logo'>Barcelona</h3>
         </div>
+        <div className='sec-home'>
+            <p>" Barcelona is a very old city in which you can feel the weight of the history; it is haunted by history. You cannot walk around it without perceiving it... "</p>
+            <br/>
+            <hr className='line'/>
+            <br/>
+            <p className='name'>Carlos Ruiz Zafon</p>
+        </div>
     </div>
     
     
     
-    <div className='sec-home'>
-    <p>" Barcelona is a very old city in which you can feel the weight of the history; it is haunted by history. You cannot walk around it without perceiving it... "</p>
-    <br/>
-    <hr className='line'/>
-    <br/>
-    <p className='name'>Carlos Ruiz Zafon</p>
-    </div>
+
     </>
     )
 }
