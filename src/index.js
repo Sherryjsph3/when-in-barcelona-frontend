cadf2e1c1f287d140a408680cb6f21c97a9411cb
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2023-03-02 20:25:40 -0500

    added fonts

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import "./styles.scss";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
+import './fonts/Mirande.ttf';
+import './fonts/Amalfi Coast.ttf';
+import './lightly Sailler dafont.otf';
 
 ReactDOM.render(
   <Router>
